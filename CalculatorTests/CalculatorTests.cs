99fd061712ab2f1e6e0989b062c3ec0b5b9272d5
Mhijazi16 <gultimate0@gmail.com>
2023-04-26 16:38:27 +0200

    2end-Requirement : added Should_HandleUnknownAmountOfNumbers(); test

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Calculator.Core;
 using Xunit; 
 using FluentAssertions;
@@ -28,7 +30,16 @@ public class CalculatorTests
     [Theory]
     [InlineData("[!\"#$%&'()*+\\./:;<=>?@[\\]^_`{|}~]",0)]
     [InlineData("[!\"#3,$%&'()*+\\./:;<=>?@[\\]^_`{|}~]1,2",6)]
+    [InlineData("100,*30,25%^&*()",155)]
     public void Should_ParseOutSymbols(string values, int expected)
+    {
+        int result = _calculator.Add(values);
+        result.Should().Be(expected);
+    }
+    [Theory]
+    [InlineData("1,2,3,4,5",15)]
+    [InlineData("-100,-200,-300",-600)]
+    public void Should_HandleUnknowAmountOfNumbers(string values, int expected)
     {
         int result = _calculator.Add(values);
         result.Should().Be(expected);
