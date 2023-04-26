5d1d3dd0eee8f4ef2a26892b80013d7744f94c1b
Mhijazi16 <gultimate0@gmail.com>
2023-04-26 16:46:03 +0200

    2end-Requirement : Refactored the by adding the Result(); method that returns the results

--- CalculatorTests/CalculatorTests.cs
+++ CalculatorTests/CalculatorTests.cs
@@ -6,16 +6,18 @@ using FluentAssertions;
 public class CalculatorTests 
 {
     private readonly Calculator _calculator;
+    private int Result(string values) => _calculator.Add(values);
     public CalculatorTests()
     {
         _calculator = new Calculator(); 
     }
     
-    [Fact]
-    public void EmptyStringOrNull_Should_ReturnZero()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void EmptyStringOrNull_Should_ReturnZero(string values)
     {
-        int result = _calculator.Add("");
-        result.Should().Be(0); 
+        Result(values).Should().Be(0); 
     }
 
     [Theory]
@@ -23,8 +25,7 @@ public class CalculatorTests
     [InlineData("1,-2,5", 4)]
     public void ShouldAddDifferentNumbbers(string values, int expected)
     {
-        int result = _calculator.Add(values);
-        result.Should().Be(expected);
+        Result(values).Should().Be(expected);
     }
 
     [Theory]
@@ -33,15 +34,16 @@ public class CalculatorTests
     [InlineData("100,*30,25%^&*()",155)]
     public void Should_ParseOutSymbols(string values, int expected)
     {
-        int result = _calculator.Add(values);
-        result.Should().Be(expected);
+        Result(values).Should().Be(expected);
     }
+    
     [Theory]
     [InlineData("1,2,3,4,5",15)]
     [InlineData("-100,-200,-300",-600)]
     public void Should_HandleUnknowAmountOfNumbers(string values, int expected)
     {
-        int result = _calculator.Add(values);
-        result.Should().Be(expected);
+        Result(values).Should().Be(expected);
     }
+
+    
 }
