759d911e7aeb9d790112d158f1ab65f9d311566e
Mhijazi16 <gultimate0@gmail.com>
2023-04-27 20:56:33 +0200

    4th-Requirement : finishing UP

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -11,7 +11,7 @@ public class Calculator
         SetUpDelimiter();
         SetUpRegex();
         
-        if (String.IsNullOrEmpty(values))
+        if (String.IsNullOrEmpty(_values))
             return 0;
         return SumUpValues();
     }
